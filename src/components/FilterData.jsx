d6c53ea31ee0a2c725fc412053f14f8a1f983f5d
TOSHIBA <faraz.m4765@gmail.com>
2022-09-19 22:07:46 +0500

    changings in the filterdata compoenet

--- src/components/FilterData.jsx
+++ src/components/FilterData.jsx
@@ -2,69 +2,69 @@ import React, { useState } from 'react'
 
 const FilterData = () => {
     const [index, setIndex] = useState('new')
-    const [val, setVal] = useState([]);
+    const [val, setVal] = useState([{
+        id: 1,
+        Post_name: "Faraz",
+        Post_Image: "xyz.img",
+        post_category: "pic",
+        price: "1500",
+        index: "new"
+    },
+    {
+        id: 2,
+        Post_name: "Ahmed",
+        Post_Image: "abc.img",
+        post_category: "pic",
+        price: "1000",
+        index: "new"
+    },
+    {
+        id: 3,
+        Post_name: "Farhan",
+        Post_Image: "def.img",
+        post_category: "document",
+        price: "2000",
+        index: "new"
+    },
+    {
+        id: 4,
+        Post_name: "kashif",
+        Post_Image: "ghi.img",
+        post_category: "document",
+        price: "3000",
+        index: "new"
+    },
+    {
+        id: 5,
+        Post_name: "Awais",
+        Post_Image: "jkl.img",
+        post_category: "document",
+        price: "3500",
+        index: "new"
+    },
+    {
+        id: 6,
+        Post_name: "Usman",
+        Post_Image: "mno.img",
+        post_category: "pic",
+        price: "4000",
+        index: "new"
+    },
+    {
+        id: 7,
+        Post_name: "Arsalan",
+        Post_Image: "pqr.img",
+        post_category: "document",
+        price: "4500",
+        index: "new"
+    }]);
 
-    const userData = [
-        {
-            id: "1",
-            Post_name: "Faraz",
-            Post_Image: "xyz.img",
-            post_category: "document",
-            price: "1500",
-            index: "new"
-        },
-        {
-            id: "2",
-            Post_name: "Ahmed",
-            Post_Image: "abc.img",
-            post_category: "document",
-            price: "1000",
-            index: "new"
-        },
-        {
-            id: "3",
-            Post_name: "Farhan",
-            Post_Image: "def.img",
-            post_category: "document",
-            price: "2000",
-            index: "new"
-        },
-        {
-            id: "4",
-            Post_name: "kashif",
-            Post_Image: "ghi.img",
-            post_category: "document",
-            price: "3000",
-            index: "new"
-        },
-        {
-            id: "5",
-            Post_name: "Awais",
-            Post_Image: "jkl.img",
-            post_category: "document",
-            price: "3500",
-            index: "new"
-        },
-        {
-            id: "6",
-            Post_name: "Usman",
-            Post_Image: "mno.img",
-            post_category: "document",
-            price: "4000",
-            index: "oldposts"
-        },
-        {
-            id: "7",
-            Post_name: "Arsalan",
-            Post_Image: "pqr.img",
-            post_category: "document",
-            price: "4500",
-            index: "oldposts"
-        },
-    ]
 
-    const handleDelete = () => {
-        userData.filter((productItem => productItem.id !== index.id))
+    const handleDelete = (id) => {
+        alert(id)
+        // const values = val
+        const dele = val.filter((item) => item.post_category !== "pic")
+        setVal(dele)
 
     }
 
@@ -72,9 +72,9 @@ const FilterData = () => {
         <div className='container col-lg-7'>
             <div className='card'>
                 <div className='card-body'>
-                    <button className='btn btn-primary' onClick={() => setIndex("new")}>new</button>
-                    <button className='btn btn-primary ms-2' onClick={() => setIndex("oldposts")}>old</button>
-                    <button className='btn btn-primary ms-2' onClick={() => setIndex("All")}>All</button>
+                    {/* <button className='btn btn-primary' onClick={() => setIndex("new")}>new</button>
+                    <button className='btn btn-primary ms-2' onClick={() => setIndex("new")}>old</button>
+                    <button className='btn btn-primary ms-2' onClick={() => setIndex("new")}>All</button> */}
 
                     <table>
                         <thead>
@@ -85,7 +85,7 @@ const FilterData = () => {
                             <th>price</th>
                         </thead>
                         <tbody>
-                            {userData.filter((objects) => objects.index === index).map((items) => {
+                            {val.map((items, indexx) => {
                                 return (
                                     <tr>
                                         <td>{items.id}</td>
@@ -93,8 +93,7 @@ const FilterData = () => {
                                         <td>{items.Post_Image}</td>
                                         <td>{items.post_category}</td>
                                         <td>{items.price}</td>
-                                        <td>{items.index}</td>
-                                        <td><button className='btn btn-primary' onClick={handleDelete} >X X</button></td>
+                                        <td><button className='btn btn-primary ms-3' onClick={() => handleDelete(items.id)} >-_-</button></td>
                                     </tr>
                                 )
                             })
